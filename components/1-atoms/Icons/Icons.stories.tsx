68dc15c04d89b335bed9de112323ad034c49bd06
Puyan Wei <puyanwei@gmail.com>
2023-06-14 15:16:09 +0100

    Add icons to Stoybook

--- components/1-atoms/Icons/Icons.stories.tsx
+++ components/1-atoms/Icons/Icons.stories.tsx
@@ -1,6 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { Icon as Component } from "."
 import { Logo } from "./Logo"
+import { Hamburger } from "./Hamburger"
+import { Close } from "./Close"
+import { Arrow } from "./Arrow"
+import { AngleLeft } from "./AngleLeft"
+import { AngleRight } from "./AngleRight"
 
 const meta: Meta<typeof Component> = {
   title: "Atoms/Icons",
@@ -12,8 +17,13 @@ type Story = StoryObj<typeof Component>
 
 export const Icons: Story = {
   render: () => (
-    <div className="w-24 bg-black">
-      <Component className="p-4" icon={<Logo />} />
+    <div className="w-24 space-y-4">
+      <Component className="p-4 bg-black" icon={<Logo />} />
+      <Component className="p-4 bg-black" icon={<Hamburger />} />
+      <Component className="p-4" icon={<Close />} />
+      <Component className="p-4" icon={<Arrow />} />
+      <Component className="p-4 bg-black" icon={<AngleLeft />} />
+      <Component className="p-4 bg-black" icon={<AngleRight />} />
     </div>
   ),
 }
